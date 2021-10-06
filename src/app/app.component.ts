1b9ff47e96f9155015815ae91522ff139c5978ac
john edwards <joedw1978@gmail.com>
2021-10-06 11:41:06 -0400

    ??

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,38 +25,39 @@ export class AppComponent {
       window.addEventListener('gamepadconnected', (e: GamepadEvent) => {
         this.gamepadIndex = e.gamepad.index;
       });
+      setInterval(() => {
+        if (this.gamepadIndex !== undefined) {
+          // a gamepad is connected and has an index
+          this.myGamepad = navigator.getGamepads()[this.gamepadIndex];
+          this.myGamepad.buttons
+            .map((e: any) => e.pressed)
+            .forEach((isPressed: any, buttonIndex: any) => {
+              if (isPressed) {
+                if (buttonIndex === 8) {
+                  this.up = true;
+                  this.down = false;
+                }
+                if (buttonIndex === 5) {
+                  this.down = true;
+                  this.up = false;
+                }
+                if (buttonIndex !== 5 && buttonIndex !== 8) {
+                  this.down = false;
+                  this.up = false;
+                }
+                // button is pressed; indicate this on the page
+                this.pressed[buttonIndex] = true;
+  
+              } else {
+                this.pressed[buttonIndex] = false;
+              }
+            });
+        }
+      }, 5);
     }
     
 
-    setInterval(() => {
-      if (this.gamepadIndex !== undefined) {
-        // a gamepad is connected and has an index
-        this.myGamepad = navigator.getGamepads()[this.gamepadIndex];
-        this.myGamepad.buttons
-          .map((e: any) => e.pressed)
-          .forEach((isPressed: any, buttonIndex: any) => {
-            if (isPressed) {
-              if (buttonIndex === 8) {
-                this.up = true;
-                this.down = false;
-              }
-              if (buttonIndex === 5) {
-                this.down = true;
-                this.up = false;
-              }
-              if (buttonIndex !== 5 && buttonIndex !== 8) {
-                this.down = false;
-                this.up = false;
-              }
-              // button is pressed; indicate this on the page
-              this.pressed[buttonIndex] = true;
-
-            } else {
-              this.pressed[buttonIndex] = false;
-            }
-          });
-      }
-    }, 5);
+    
   }
 
   trackByFn(index: number) {

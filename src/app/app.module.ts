d02bd183239df027d0f80e6f25320554d696d9a0
john edwards <joedw1978@gmail.com>
2021-10-06 11:49:13 -0400

    try this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({
+      appId: 'ng-universal-heroku-demo'
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
